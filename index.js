9a412844e6b5e567c3ddb2c71aeaa2531e283050
KJ <kris@krjo.io>
2020-03-10 09:50:39 -0700

    sets up post request handler for wishlist

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8080;
+const dotenv = require('dotenv').config();
+const Shopify = require('shopify-api-node');
+
+app.use(bodyParser.json());
+
+const shopify = new Shopify({
+  shopName: process.env.SHOP_NAME,
+  apiKey: process.env.API_KEY,
+  password: process.env.PASSWORD
+});
+
+app.post('/',(req, res) => {
+  console.info('REQUEST BODY',req.body);
+
+  const id = parseInt(req.body.customerId);
+  const products = req.body.products;
+
+  shopify.metafield
+  .create({
+    key: 'wishlist',
+    value: products,
+    value_type: 'string',
+    namespace: 'puravida',
+    owner_resource: 'customer',
+    owner_id: id
+  })
+  .then(
+    (metafield) => console.log('SUCCESS',metafield),
+    (err) => console.error(err)
+  );
+
+  if (id && products) {
+    return res.status(200).send({
+      success: 'true',
+      message: 'Customer wishlist successfully updated.'
+    });
+  } else {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Missing param, failed to update customer wishlist.'
+    });
+  }
+});
+
+app.listen(port,() => {
+  console.log(`Server running on port ${port}`)
+});
