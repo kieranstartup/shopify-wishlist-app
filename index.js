50f34f384d532e2aefacf24d7b186543bab14c29
KJ <kris@krjo.io>
2020-03-13 13:25:20 -0700

    adds sentry reporting to wishlist"

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 const dotenv = require('dotenv').config();
 const Shopify = require('shopify-api-node');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://7afc5783087d4758819f1faff7ef28d3@sentry.io/4617472' });
 
 const shopify = new Shopify({
   shopName: process.env.SHOP_NAME,
@@ -35,6 +38,7 @@ app.post('/',(req, res) => {
   .then(
     (metafield) => {
       console.log(metafield);
+      Sentry.captureMessage(`Customer ${id} updated their wishlist: "${products}".`);
       return res.status(200).send({
         success: 'true',
         message: 'Customer wishlist successfully updated.'
@@ -42,6 +46,7 @@ app.post('/',(req, res) => {
     },
     (err) => {
       console.error(err);
+      Sentry.captureException(err);
       return res.status(400).send({
         success: 'false',
         message: err
